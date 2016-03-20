acbd267432bcb9df6c615cdb98cb28995a85e8de
Kevin Peters <kwpeters@gmail.com>
2016-03-20 13:11:48 -0400

    Finished 2.2.

---
+++ thinking-in-react/02_build_static_part_2/program.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const FilterableProductTable = React.createClass(
+    {
+        render() {
+            return (<SearchBar></SearchBar>);
+        }
+    }
+);
+
+export const SearchBar = React.createClass(
+    {
+        render() {
+            return (<div></div>);
+        }
+    }
+);
+
