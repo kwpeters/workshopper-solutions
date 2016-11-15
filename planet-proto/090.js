1a6ad59152375f1d9fb480557205d9d1649e0e2b
Kevin Peters <kwpeters@gmail.com>
2016-11-14 21:04:22 -0500

    Finished planet-proto exercise 9.

---
+++ planet-proto/090.js
@@ -0,0 +1,26 @@
+// -> Define two constructor functions: 'Robot' and 'Vehicle'
+// -> When called with 'new', the Robot constructor function should return 
+//    the implicit 'this'
+// -> When called with 'new', the Vehicle constructor function should return 
+//    an object of your own making, not the implicit 'this'.
+
+const Robot = function Robot() {
+    "use strict";
+
+    return this;
+};
+
+const Vehicle = function Vehicle() {
+    "use strict";
+
+    return {foo: "bar"};
+};
+
+
+
+// ------------------------------------------------
+// Common JS exports for verification, don't modify
+module.exports = {
+	Robot:    Robot,
+	Vehicle:  Vehicle
+}
